b12a165f151ec763d2c333ae144717f4b44e3410
AllyssonLPereira <allyssonlunguinho.03i@gmail.com>
2024-12-19 11:04:48 -0300

    Adicionando estados ao jogo.

--- pacman/pacman2.py
+++ pacman/pacman2.py
@@ -20,8 +20,11 @@ WHITE: tuple[int, int, int] = (255, 255, 255)
 BLACK: tuple[int, int, int] = (0, 0, 0) # Background color
 RED: tuple[int, int, int] = (255, 0, 0)
 GREEN: tuple[int, int, int] = (0, 255, 0)
+ORANGE: tuple[int, int, int] = (255, 165, 0)
 YELLOW: tuple[int, int, int] = (255, 255, 0) # Color for Pac-Man
 BLUE: tuple[int, int, int] = (0, 0, 255) #
+CYAN: tuple[int, int, int] = (0, 255, 255)
+ROSE: tuple[int, int, int] = (241, 126, 161)
 
 # Velocidade dos personagens
 SPEED: int = 1 # Movement speed
@@ -77,6 +80,7 @@ class Scenario(GameElements):
         self.pacman = pac
         self.characters = []
         self.score = 0
+        self.status = 1 # 0 (pausado), 1 (jogando), 2 (game_over), 3 (vitória)
         self.size = size
         self.matrix = [
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -128,6 +132,14 @@ class Scenario(GameElements):
         return directions
 
     def calculate_rules(self):
+        if self.status == 1:
+            self.calculate_rules_playing()
+        elif self.status == 0:
+            self.calculate_rules_paused()
+        elif self.status == 2:
+            self.calculate_rules_game_over()
+
+    def calculate_rules_playing(self):
         for character in self.characters:
             line = int(character.line)
             column = int(character.column)
@@ -139,13 +151,52 @@ class Scenario(GameElements):
             if len(directions) >= 3:
                 character.paths(directions)
 
-            if 0 <= column_intention < 28 and 0 <= line_intention < 29 and \
-                    self.matrix[line_intention][column_intention] != 1:
-                character.accept_movement()
+            if isinstance(character, Ghost) and \
+                    character.line == self.pacman.line and character.column == self.pacman.column:
+                self.status = 2
             else:
-                character.refuse_movement(directions)
+                if 0 <= column_intention < 28 and 0 <= line_intention < 29 and \
+                        self.matrix[line_intention][column_intention] != 1:
+                    character.accept_movement()
 
-    def paint(self, screen: SurfaceType):
+                    if isinstance(character, Pacman) and self.matrix[line][column] == 0:
+                        self.score += 1
+                        self.matrix[line][column] = 2
+
+                        if self.score >= 306:
+                            self.status = 3
+
+                else:
+                    character.refuse_movement(directions)
+
+    def calculate_rules_paused(self):
+        pass
+
+    def calculate_rules_game_over(self):
+        pass
+
+    def paint(self, screen):
+        if self.status == 1:
+            self.paint_playing(screen)
+        elif self.status == 0:
+            self.paint_playing(screen)
+            self.paint_paused(screen)
+        elif self.status == 2:
+            self.paint_playing(screen)
+            self.paint_game_over(screen)
+        elif self.status == 3:
+            self.paint_playing(screen)
+            self.paint_victory(screen)
+
+    @staticmethod
+    def paint_text_status(screen, text):
+        img_text = font.render(text, True, YELLOW)
+        position_line = ((screen.get_width() - img_text.get_width()) // 2)
+        position_column = ((screen.get_height() - img_text.get_height()) // 2)
+
+        screen.blit(img_text, (position_line, position_column))
+
+    def paint_playing(self, screen: SurfaceType):
         """
 
         :type screen: SurfaceType
@@ -153,6 +204,15 @@ class Scenario(GameElements):
         for row_number, row in enumerate(self.matrix):
             self.paint_matrix(screen, row_number, row)
         self.paint_score(screen)
+
+    def paint_paused(self, screen):
+        self.paint_text_status(screen, "P A U S E D")
+
+    def paint_game_over(self, screen):
+        self.paint_text_status(screen, "GAME OVER")
+
+    def paint_victory(self, screen):
+        self.paint_text_status(screen, "VICTORY")
 
     def paint_matrix(self, screen: SurfaceType, row_number: int, row: list[int | Any] | Any):
         """
@@ -191,6 +251,12 @@ class Scenario(GameElements):
         for event in events:
             if event.type == pygame.QUIT:
                 exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    if self.status == 0:
+                        self.status = 1
+                    else:
+                        self.status = 0
 
 
 class Pacman(GameElements, Moviments):
@@ -208,6 +274,7 @@ class Pacman(GameElements, Moviments):
         self.radius: int = self.size // 2
         self.column_intention: int = self.column
         self.line_intention: int = self.line
+        self.abertura = 0
 
     def calculate_rules(self):
         self.column_intention = self.column + self.speed_x
@@ -234,10 +301,15 @@ class Pacman(GameElements, Moviments):
         # draw pacman's body
         pygame.draw.circle(surface, YELLOW, (self.center_x, self.center_y), self.radius, 0)
 
+        self.abertura += 1
+
+        if self.abertura > self.radius:
+            self.abertura = 0
+
         # draw the mouth
         corner_mouth: tuple[int, int] = (self.center_x, self.center_y)
-        upper_lip: tuple[int, int] = (self.center_x + self.radius, self.center_y - self.radius)
-        lower_lip: tuple[int, int] = (self.center_x + self.radius, self.center_y)
+        upper_lip: tuple[int, int] = (self.center_x + self.radius, self.center_y - self.abertura)
+        lower_lip: tuple[int, int] = (self.center_x + self.radius, self.center_y + self.abertura)
 
         points: list[tuple[int, int]] = [corner_mouth, upper_lip, lower_lip]
 
@@ -266,8 +338,8 @@ class Pacman(GameElements, Moviments):
 
 class Ghost(GameElements):
     def __init__(self, size, color):
-        self.column = 6.0
-        self.line = 2.0
+        self.column = 13.0
+        self.line = 15.0
         self.column_intention = self.column
         self.line_intention = self.line
         self.speed = 1
@@ -344,21 +416,46 @@ class Ghost(GameElements):
 
 
 if __name__ == "__main__":
+    # Instanciando o pacman
     pacman: Pacman = Pacman(SIZE)
+
+    # Instanciando os fantasmas
     blinky = Ghost(SIZE, RED)
-    scenario: Scenario = Scenario(SIZE, pacman, blinky)
+    inky = Ghost(SIZE, CYAN)
+    clyde = Ghost(SIZE, ORANGE)
+    pinky = Ghost(SIZE, ROSE)
+
+    # Instanciando o cenário, passando os personagens
+    scenario: Scenario = Scenario(SIZE, pacman)
+    scenario.add_characters(pacman)
+    scenario.add_characters(blinky)
+    scenario.add_characters(inky)
+    scenario.add_characters(clyde)
+    scenario.add_characters(pinky)
 
     while True:
         # calculate the rules
         pacman.calculate_rules()
         blinky.calculate_rules()
+        inky.calculate_rules()
+        clyde.calculate_rules()
+        pinky.calculate_rules()
         scenario.calculate_rules()
 
         # paint the screen
         screen.fill(BLACK)
         scenario.paint(screen)
+
+        # pintando o pacman
         pacman.paint(screen)
+
+        # pintando os fantasmas
         blinky.paint(screen)
+        inky.paint(screen)
+        clyde.paint(screen)
+        pinky.paint(screen)
+
+        # Atualizando o cenário e colocando delay
         pygame.display.update()
         pygame.time.delay(50)
 
